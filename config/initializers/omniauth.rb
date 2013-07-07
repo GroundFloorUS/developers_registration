9fb1deaa8b22ee9ea4c1675a79a24b8837c098f9
Chris Schmitt <billsfannc@gmail.com>
2013-07-07 15:22:47 -0400

    - Updated tokens

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, Figaro.env.TWITTER_KEY, Figaro.env.TWITTER_SECRET
   #provider :linkedin, Figaro.env.LINKEDIN_KEY, Figaro.env.LINKEDIN_SECRET, :scope => 'r_fullprofile r_emailaddress r_network', :fields => ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "connections"]
   provider :linkedin, Figaro.env.LINKEDIN_KEY, Figaro.env.LINKEDIN_SECRET
-  provider :facebook, Figaro.env.FACEBOOK_KEY, Figaro.env.FACEBOOK_SECRET, :scope => 'email,user_birthday', :display => 'popup'
+  provider :facebook, Figaro.env.FACEBOOK_KEY, Figaro.env.FACEBOOK_SECRET, :scope => 'email,user_birthday'
 end
