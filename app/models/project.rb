b985b2a8113ddf6bd9c8542e5aa1131c379135c4
Chris Schmitt <billsfannc@gmail.com>
2013-07-07 22:36:21 -0400

    - Fixed registration redirect bug - Updated tokens - Added application.yml to gitignore

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   
   CATEGORIES = ["Single Family For Sale", "Single Family For Lease", "Duplex", "Multifamily", "Apartment Complex", "Land", "Retail", "Office", "Restaurant", "Lodging / Resort", "Other"]
   AMOUNT_TO_RAISE = ["50-250K", "250K-$1M", "$1-5M", ">$5M"]
-  CAPTIAL_TYPE =  ["Debt",  "Equity"]
+  CAPTIAL_TYPE =  ["Debt",  "Equity", "Both"]
   LOAN_TO_VALUE = ["60%","70%","80%","90%"]
   CLOSE_TIMELINE = ["<30 days", "30-60 days", "60-90 days", "90-120 days", ">120 days", "Flexible / Doesn\'t Matter"]
   
